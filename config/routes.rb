6b82c1e2ca72837c03d7fa9cf51c1b71604f102b
noobiehacker <davidkwokhochan@gmail.com>
2016-03-15 16:07:08 -0700

    Fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :coinsaves
-  resources :coinusers
   resources :wallets
   resources :coinusers
-  root "controller#action"
+  root to: 'coinusers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
