45854af70c5d4dc1d1ebf193b728021c769c02d4
noobiehacker <davidkwokhochan@gmail.com>
2016-03-15 17:23:56 -0700

    Add a Static Pages controller

--- Gemfile
+++ Gemfile
@@ -44,7 +44,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 gem 'rails_12factor', group: :production
 
 ruby "2.2.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :coinsaves
   resources :wallets
   resources :coinusers
-  root to: 'coinusers#index'
+#  root 'coinusers#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
