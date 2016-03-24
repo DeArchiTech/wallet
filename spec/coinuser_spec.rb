0e34ee96195655f7f92c62efeeaaa8648c3aba3e
noobiehacker <davidkwokhochan@gmail.com>
2016-03-23 18:27:04 -0700

    added coin user spec

--- spec/coinuser_spec.rb
+++ spec/coinuser_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 describe Coinuser do
-	it "is valid with a firstname, lastname, email"
-	it "is invalid without an email"
-	it "is invalid without an email address"
+	it "is valid with a firstname, lastname, email" do	
+   	  expect(written).to eq true	
+	end
+	it "is invalid without an email"do 
+   	  expect(written).to eq true	
+	end
+	it "is invalid without an email address" do
+   	  expect(written).to eq true	
+	end
+	it "creates a new coinuser in the database" do	
+   	  expect(written).to eq true
+  	end
+	it "reads a coinuser from the database" do	
+   	  expect(written).to eq true	
+	end
+	it "updates a coinuser in the database" do
+   	  expect(written).to eq true	
+	end
+	it "deletes a coinuser in the database" do
+   	  expect(written).to eq true	
+	end
 end
